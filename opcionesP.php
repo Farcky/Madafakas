d6cea34732b2d8009c66b915487d577af0d73d58
Farcky <erickromerosanchez27@gmail.com>
2023-05-16 15:38:06 -0600

    Prueba

---
+++ opcionesP.php
@@ -0,0 +1,204 @@
+<?php include "head.php"; ?>
+<body id="page-top">
+
+       <div id="wrapper">
+         <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
+         <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+               <div class="sidebar-brand-icon rotate-n-12">
+                    <i class="fas fa-search"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">Panel<sup>Control</sup></div>
+            </a>
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <?php  
+            include "muestra_menu.php"; 
+                    ?>
+                      <!-- Divider -->
+            <hr class="sidebar-divider"> 
+
+</ul>
+
+<div id="content-wrapper" class="d-flex flex-column">
+
+
+<!-- Main Content -->
+<div id="content">
+
+
+    <!-- Topbar -->
+    <nav class="navbar navbar-expand navbar-light  bg-white topbar mb-4 static-top shadow">
+
+        <!-- Sidebar Toggle (Topbar) -->
+        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+        </button>
+
+        <!-- Topbar Search -->
+        <form
+            class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+            <div class="input-group">
+                <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                    aria-label="Search" aria-describedby="basic-addon2">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="button">
+                        <i class="fas fa-search fa-sm"></i>
+                    </button>
+                </div>
+            </div>
+        </form>
+
+        <!-- Topbar Navbar -->
+        <ul class="navbar-nav ml-auto">
+
+            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+            <li class="nav-item dropdown no-arrow d-sm-none">
+                <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-search fa-fw"></i>
+                </a>
+                <!-- Dropdown - Messages -->
+                <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+                    aria-labelledby="searchDropdown">
+                    <form class="form-inline mr-auto w-100 navbar-search">
+                        <div class="input-group">
+                            <input type="text" class="form-control bg-light border-0 small"
+                                placeholder="Search for..." aria-label="Search"
+                                aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-primary" type="button">
+                                    <i class="fas fa-search fa-sm"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </li>
+
+            <div class="topbar-divider d-none d-sm-block"></div>
+
+            <!-- Nav Item - User Information -->
+            <li class="nav-item dropdown no-arrow">
+                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">User</span>
+                    <img class="img-profile rounded-circle"
+                        src="img/undraw_profile.svg">
+                </a>
+                <!-- Dropdown - User Information -->
+                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                    aria-labelledby="userDropdown">
+                    <a class="dropdown-item" href="#">
+                        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                        Profile
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                        Logout
+                    </a>
+                </div>
+            </li>
+
+        </ul>
+
+    </nav>
+
+    <div class="container-fluid">
+
+    <div class="col-xl-13 col-lg-13">
+                            <div class="card shadow mb-4">
+                                <!-- Card Header - Dropdown -->
+                                <div
+                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-primary">Opciones Perfil</h6>
+                                    <div class="dropdown no-arrow">
+                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                           
+                                        </a>
+                                    </div>
+                                </div>
+                                <!-- Card Body -->
+                                <div class="card-body">
+                                    
+                                <div class="container text-left">
+  <div class="row">
+    <div class="col">
+    <table class="table">
+   
+        <thead>
+            <tr>
+                <th>Perfil</th>
+                <th>Opciones</th>
+                <th>Funciones</th>
+                
+            </tr>
+            </thead>
+            <tbody>
+                    
+                    <tr>
+                    <td>1</td>
+                    <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header">
+      <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <p>1<input type="checkbox">Dashboard</p>
+                    <p>2<input type="checkbox"></p>
+                    <p>3<input type="checkbox"></p>
+                    <p>4<input type="checkbox"></p>
+                    <p>5<input type="checkbox"></p>
+                    <p>6<input type="checkbox"></p>
+                    <p>7<input type="checkbox"></p>
+                    <p>8<input type="checkbox"></p>
+                    <p>9<input type="checkbox"></p>
+                    <p>10<input type="checkbox"></p>
+                    <p>11<input type="checkbox"></p>
+                    <p>12<input type="checkbox"></p>
+                    <p>13<input type="checkbox"></p>
+                    <p>14<input type="checkbox"></p>
+                    <p>15<input type="checkbox"></p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div></td>
+<td></td>
+</table>
+
+                    
+    
+    
+   
+
+    </div>
+  </div>
+ </div>
+</div>
+</div>
+
+  
+                   
+                                </div>
+
+</div>
+
+
+                
+             
+ 
